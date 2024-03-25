2da20a5aa1106de285ebe01ff34f8d0530aa05de
Daniel-SODISA <daniel.cruz@sodisa.online>
2024-03-25 16:49:20 -0600

    Practice componentts

---
+++ guitarla/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    const myValue = 69;
+    return (
+        <>
+        <h1>Header example</h1>
+        <h1>Example of reading a JS variable {myValue}</h1>
+        </>
+    );
+}
+
