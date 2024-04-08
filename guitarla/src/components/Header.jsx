7b91803d4e80435047e5983e1cc0484c397f166f
Spartan145 <dany145@hotmail.es>
2024-04-08 17:19:42 -0600

    Improved cart component

--- guitarla/src/components/Header.jsx
+++ guitarla/src/components/Header.jsx
@@ -1,4 +1,4 @@
-export default function Header(){
+export default function Header({cart}){
     return (
         <>
             <header className="py-5 header">
@@ -15,7 +15,7 @@ export default function Header(){
                             >
                                 <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
 
-                                <div id="carrito" className="bg-white p-3">
+                                <div id="carrito" className="bg-white p-3">                                
                                     <p className="text-center">El carrito esta vacio</p>
                                     <table className="w-100 table">
                                         <thead>
@@ -28,38 +28,40 @@ export default function Header(){
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
-                                                <td>
-                                                    <img className="img-fluid" src="./public/img/guitarra_02.jpg" alt="imagen guitarra" />
-                                                </td>
-                                                <td>SRV</td>
-                                                <td className="fw-bold">
-                                                        $299
-                                                </td>
-                                                <td className="flex align-items-start gap-4">
-                                                    <button
-                                                        type="button"
-                                                        className="btn btn-dark"
-                                                    >
-                                                        -
-                                                    </button>
-                                                        1
-                                                    <button
-                                                        type="button"
-                                                        className="btn btn-dark"
-                                                    >
-                                                        +
-                                                    </button>
-                                                </td>
-                                                <td>
-                                                    <button
-                                                        className="btn btn-danger"
-                                                        type="button"
-                                                    >
-                                                        X
-                                                    </button>
-                                                </td>
-                                            </tr>
+                                            {cart.map(item =>(
+                                                <tr key={item.id}>
+                                                    <td>
+                                                        <img className="img-fluid" src={`/img/${item.image}.jpg`} alt="imagen guitarra" />
+                                                    </td>
+                                                    <td>{item.name}</td>
+                                                    <td className="fw-bold">
+                                                            {item.price}
+                                                    </td>
+                                                    <td className="flex align-items-start gap-4">
+                                                        <button
+                                                            type="button"
+                                                            className="btn btn-dark"
+                                                        >
+                                                            -
+                                                        </button>
+                                                            {item.quantity}
+                                                        <button
+                                                            type="button"
+                                                            className="btn btn-dark"
+                                                        >
+                                                            +
+                                                        </button>
+                                                    </td>
+                                                    <td>
+                                                        <button
+                                                            className="btn btn-danger"
+                                                            type="button"
+                                                        >
+                                                            X
+                                                        </button>
+                                                    </td>
+                                                </tr>
+                                            ))}                                            
                                         </tbody>
                                     </table>
 
