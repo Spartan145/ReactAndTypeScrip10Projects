54585e478eab14c741119f5680a2be28fa3375e2
Daniel-SODISA <daniel.cruz@sodisa.online>
2024-04-11 17:00:23 -0600

    implement useMemo hook

--- guitarla/src/components/Header.jsx
+++ guitarla/src/components/Header.jsx
@@ -1,7 +1,9 @@
+import { useMemo } from "react";
+
 export default function Header({cart}){
     //Derived State
-    const isEmpty = () => cart.length === 0;
-    const cartTotal = () => cart.reduce((total, item) => total + (item.quantity * item.price), 0);
+    const isEmpty = useMemo(() => cart.length === 0);
+    const cartTotal = useMemo(() => cart.reduce((total, item) => total + (item.quantity * item.price), 0));
     
     return (
         <>
@@ -19,7 +21,7 @@ export default function Header({cart}){
                             >
                                 <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
                                 <div id="carrito" className="bg-white p-3"> 
-                                    {isEmpty()? (
+                                    {isEmpty? (
                                         <p className="text-center">El carrito esta vacio</p>
                                     ) : (
                                         <>
@@ -70,7 +72,7 @@ export default function Header({cart}){
                                                 ))}                                            
                                             </tbody>
                                         </table>
-                                        <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal()}</span></p>
+                                        <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal}</span></p>
                                         <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
                                         </>
                                     )}                                                                                                       
