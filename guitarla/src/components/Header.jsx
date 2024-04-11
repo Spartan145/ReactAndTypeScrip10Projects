68c58cdfbe72ecff8e2815cfc7659597bba4e8fe
Daniel-SODISA <daniel.cruz@sodisa.online>
2024-04-11 16:48:12 -0600

    Generate cart total

--- guitarla/src/components/Header.jsx
+++ guitarla/src/components/Header.jsx
@@ -1,6 +1,7 @@
 export default function Header({cart}){
     //Derived State
     const isEmpty = () => cart.length === 0;
+    const cartTotal = () => cart.reduce((total, item) => total + (item.quantity * item.price), 0);
     
     return (
         <>
@@ -69,7 +70,7 @@ export default function Header({cart}){
                                                 ))}                                            
                                             </tbody>
                                         </table>
-                                        <p className="text-end">Total pagar: <span className="fw-bold">$899</span></p>
+                                        <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal()}</span></p>
                                         <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
                                         </>
                                     )}                                                                                                       
