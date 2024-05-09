24a9922575a4d92c428a25f85875d1bb19e1affb
Daniel-SODISA <daniel.cruz@sodisa.online>
2024-05-09 14:51:20 -0600

    test assertion operator

--- guitarla-ts/src/main.tsx
+++ guitarla-ts/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(//Use ! as assertion operator to ensure TS we will return a determined type of field
   <React.StrictMode>
     <App />
   </React.StrictMode>,
