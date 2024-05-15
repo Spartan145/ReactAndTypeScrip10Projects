32b17846338ce69203de5945955b34e7a75d2d6f
Daniel-SODISA <daniel.cruz@sodisa.online>
2024-05-15 15:23:04 -0600

    Fixed dark mode toggle

--- tip_calculator/src/App.tsx
+++ tip_calculator/src/App.tsx
@@ -1,11 +1,15 @@
-import { Textarea } from "@/components/ui/textarea"
+import { ThemeProvider } from "@/components/theme-provider"
+import { ModeToggle } from "./components/mode-toggle"
 
 function App() {  
 
-  return (
-    <>
-      <Textarea />
-    </>
+  return (        
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <header className="bg-orange-400 py-5">
+      <h1 className="text-base font-black">Hola mundo</h1>
+      <ModeToggle></ModeToggle>   
+      </header>                  
+    </ThemeProvider>        
   )
 }
 
