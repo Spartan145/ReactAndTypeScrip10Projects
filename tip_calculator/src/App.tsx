c3894c9d00cc4f9708b2242d68bdfecb1511c327
Daniel-SODISA <daniel.cruz@sodisa.online>
2024-05-15 15:56:59 -0600

    Created custom header

--- tip_calculator/src/App.tsx
+++ tip_calculator/src/App.tsx
@@ -5,10 +5,17 @@ function App() {
 
   return (        
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <header className="bg-orange-400 py-5">
-      <h1 className="text-base font-black">Hola mundo</h1>
-      <ModeToggle></ModeToggle>   
-      </header>                  
+      <div className="flex min-h-screen w-full flex-col">
+      <header className="sticky top-0 flex h-14 items-center gap-4 border-b bg-background px-4 md:px-6">
+        <h5 className="flex-1 text-center text-2xl font-black">TIPS CALCULATOR</h5>
+        <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+          <ModeToggle></ModeToggle>
+        </nav>                
+      </header>
+      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+        
+      </main>
+    </div>
     </ThemeProvider>        
   )
 }
