a28cda6fdfd180de46f1ddfdb7effd27bccebe76
Daniel-SODISA <daniel.cruz@sodisa.online>
2024-06-26 16:51:47 -0600

    Calculate total amount

--- tip_calculator/src/components/OrderTotals.tsx
+++ tip_calculator/src/components/OrderTotals.tsx
@@ -11,12 +11,13 @@ type OrderTotalsProps = {
 export default function OrderTotals({order, tip} : OrderTotalsProps) {
     const subTotalAmount = useMemo(() => order.reduce((accum,item) => accum + (item.price * item.quantity), 0),[order])
     const tipAmount = useMemo(() => subTotalAmount * tip,[tip, order])
+    const totalAmount = useMemo(() => subTotalAmount + tipAmount,[tip, order])
 
     return (
         <>
             <p className="text-xl text-left font-extrabold w-full">Subtotal: {formatCurrency(subTotalAmount)}</p>                        
             <p className="text-xl text-left font-extrabold w-full">Tip: {formatCurrency(tipAmount)}</p>
-            <p className="text-xl text-left font-extrabold w-full">Total: </p>
+            <p className="text-xl text-left font-extrabold w-full">Total: {formatCurrency(totalAmount)}</p>
         </>        
     )
 }
